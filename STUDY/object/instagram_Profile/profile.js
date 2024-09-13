cb280d531fcaf1295cae00d46eff994ed04dd4a6
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-09-13 12:24:25 +0530

    Instagram

---
+++ STUDY/object/instagram_Profile/profile.js
@@ -0,0 +1,11 @@
+const instagram = {
+    userName: "mr_abhi_930",
+    isFollow : true,
+    post : 0,
+    followers : 350,
+    following : 176,
+    name: "Abhishek",
+    threadID: "@mr_abhi_930",
+    profession : "Trader",
+    bio : "I constantly aspire to improve myself ",
+};console.log(instagram)
