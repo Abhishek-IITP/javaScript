d1d8961e66b156178563b458030933b903763501
Abhishek Kumar Mohanty <165031056+Abhishek-IITP@users.noreply.github.com>
2024-09-20 12:27:55 +0530

    Update index.js

--- Calculator/js/index.js
+++ Calculator/js/index.js
@@ -12,22 +12,18 @@ for (let i = 0; i < keys.length; i++) {
             decimalAdded = false; 
         }
 
-        // If the '=' button is clicked, do the math
         else if (btnValue == '=') {
-            let equation = input; // Get the math equation from the screen
-            let lastChar = equation[equation.length - 1]; // Last thing typed
+            let equation = input; 
+            let lastChar = equation[equation.length - 1]; 
 
-            // Change x to * and ÷ to / because that's how we do math in code
             equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
 
-            // If the last thing typed is a symbol or a dot, remove it
             if (operators.indexOf(lastChar) > -1 || lastChar == '.')
                 equation = equation.slice(0, -1);
 
-            // Now, do the math and show the result on the screen
             if (equation) screen.innerHTML = eval(equation);
 
-            decimalAdded = false; // Reset the dot
+            decimalAdded = false; 
         }
         else  {
             screen.innerHTML += btnValue;
