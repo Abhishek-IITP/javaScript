8cc47b514d6251214384c9235c985cd0c79b0b12
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-09-27 18:40:44 +0530

    Create

---
+++ STUDY/Scope/lexical Scoping/indexx.js
@@ -0,0 +1,16 @@
+// var name="ABhishek";
+// function fun1(){
+//     var name="Abhi";
+//     console.log(name)
+// }
+// fun1();
+
+/**output="Abhi" */
+
+
+function fun1(){
+    console.log(name);
+    var name ="Abhishek";
+    console.log(name);
+}
+fun1()
