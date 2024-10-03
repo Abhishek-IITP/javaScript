8f48eea681d346e9ad07c35066b29ad8025591b8
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-10-03 20:49:19 +0530

    Create

---
+++ STUDY/DSA/sorting/insertion_sort.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i];
+        let j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+
+let arr3 = [12, 11, 13, 5, 6];
+console.log(insertionSort(arr3));
