36f872d8fda6c6d753c706ada8264577a5d10de5
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-10-03 20:49:06 +0530

    Create

---
+++ STUDY/DSA/sorting/Bubble_sort_algo.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+          if (arr[j] > arr[j + 1]) {
+              // Swap arr[j] and arr[j+1]
+              let temp = arr[j];
+              arr[j] = arr[j + 1];
+              arr[j + 1] = temp;
+          }
+      }
+  }
+  return arr;
+}
+
+let arr = [64, 34, 25, 12, 22, 11, 90];
+console.log(bubbleSort(arr));
+
+  
