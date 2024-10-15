492162a076d0f5dd03d3c46e6b1e77507d181d63
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-10-15 21:13:15 +0530

    update

--- STUDY/Scope/lexical Scoping/01/indexx.js
+++ STUDY/Scope/lexical Scoping/01/indexx.js
@@ -8,9 +8,51 @@
 /**output="Abhi" */
 
 
-function fun1(){
-    console.log(name);
-    var name ="Abhishek";
-    console.log(name);
+// function fun1(){
+//     console.log(name);
+//     var name ="Abhishek";
+//     console.log(name);
+// }
+// fun1()
+
+
+
+// var naam ="Sleeping";
+// function fun1(){
+//     console.log(naam);
+    // hello ="Abhishek";  /** concept of auto global */
+//     console.log(hello)
+// }
+// fun1();
+// console.log(naam)
+
+// var naam ="Sleeping";
+// function fun1(){
+//     console.log(naam);
+//     hello ="Abhishek";
+    
+// }
+// console.log(hello) /**auto global banane ka chance hi ni mila */
+// fun1();
+// console.log(naam)
+
+var naam ="Sleeping";
+function fun2()
+
+{
+    console.log(naam)
+    function fun1(){
+    console.log(naam);
+    hello ="Abhishek";
+    
 }
-fun1()
+
+fun1();
+console.log(hello);
+console.log(naam)
+
+}
+fun2();
+
+console.log(hello);
+console.log(naam)
