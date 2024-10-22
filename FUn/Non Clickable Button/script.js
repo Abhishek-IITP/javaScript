19a72fc1a646be0cc96e5b6f8b040b0201cfc665
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-10-22 20:54:04 +0530

    Update

--- FUn/Non Clickable Button/script.js
+++ FUn/Non Clickable Button/script.js
@@ -1,7 +1,6 @@
 const noButton = document.getElementById('noButton');
 const yesButton = document.getElementById('yesButton');
 
-// When the 'No' button is clicked, move it to a random position
 noButton.addEventListener('click', function () {
     const randomX = Math.floor(Math.random() * (window.innerWidth - noButton.offsetWidth));
     const randomY = Math.floor(Math.random() * (window.innerHeight - noButton.offsetHeight));
@@ -9,7 +8,6 @@ noButton.addEventListener('click', function () {
     noButton.style.top = randomY + 'px';
 });
 
-// When the 'Yes' button is clicked, redirect to a page with a love message
 yesButton.addEventListener('click', function () {
     window.location.href = 'love.html'; // This is the page it will redirect to
 });
