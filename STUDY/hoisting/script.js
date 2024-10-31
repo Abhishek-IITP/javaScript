701c3b270b2dca3c38eb95e68c50c253aeaaef67
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-10-31 18:08:37 +0530

    Create script.js

---
+++ STUDY/hoisting/script.js
@@ -0,0 +1,51 @@
+// var name="abhi"
+
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${name} ${i}`)
+// }
+
+// const fun1 =() =>{
+//     return "Abhishek";
+// }
+
+// console.log(fun1())
+
+
+// const name="abhi";
+
+// const fun1 =() =>{
+//     return {"name":"Abhishek"};
+// }
+
+// console.log(fun1())
+
+
+// function fun1(){
+//     console.log("Function One")
+// }
+// fun1()
+
+
+
+// console.log(fun1())
+// function fun1(){
+//     console.log("Function One")
+//     return 2;
+// }
+// console.log(fun1())
+
+
+// const arrowFun =() =>{
+//     console.log("arrow function")
+// }
+
+// arrowFun()
+
+
+// arrowFun()
+// const arrowFun =() =>{
+//     console.log("arrow function")
+// }
+
+ 
