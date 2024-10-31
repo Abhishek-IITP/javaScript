17569c453220824f7f5a593dd30d1fcdf87996f4
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-10-31 18:08:30 +0530

    Create script.js

---
+++ STUDY/hoisting/ask/script.js
@@ -0,0 +1,52 @@
+// console.log(fun1())
+function fun1(){
+    console.log("Function One")
+}
+console.log(fun1())
+
+
+/**output me undefine kyu AA rha h?? */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(fun1())
+function fun1(){
+    console.log("Function One")
+    return 2;
+}
+console.log(fun1())
+
+/**kyu ki pahle case me function kuch bhi return ni kr rha tha */
