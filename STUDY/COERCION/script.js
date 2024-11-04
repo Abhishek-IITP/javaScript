f3c9d37f8e91cbb0520be393d557db06c30c9bc7
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-11-04 19:41:51 +0530

    Update script.js

--- STUDY/COERCION/script.js
+++ STUDY/COERCION/script.js
@@ -8,8 +8,16 @@
 
 /** N0W I CHANGE THE TYPE MANUALLY AND THE TYPE OF C BECOME NUMBER*/
 
-let a= 6;
-let b= Number("5");
-let c= a+b;
-console.log(c)
-console.log(typeof c)
+// let a= 6;
+// let b= Number("5");
+// let c= a+b;
+// console.log(c)
+// console.log(typeof c)
+
+
+
+console.log(9+8+5+5+2+"154"+5+5)
+console.log(typeof(9+8+5+5+2+"154"+5+5))
+console.log(6-"55")
+console.log(4+ true)
+console.log(5- false)
