5cbff7d1ee196c61f2109169c6273e2950174377
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-11-05 03:32:37 +0530

    create

---
+++ STUDY/DOM/script.js
@@ -0,0 +1,38 @@
+// const heading = document.getElementById('heading');
+// console.log(heading.innerText);
+// heading.innerText= "Kya haal h bhai ke";
+
+// heading.style.backgroundColor="pink";
+
+
+// heading.setAttribute("class", "heading helloji")
+
+/**ABB ISKA 2 ID BAN GYA H HUMNE HEADING KO DUBARA ISILIYE LIKHA TAKI NEW WALI ID PURANE ID KO REPLACE NA KR DE */
+
+
+// const newt = document.createElement("p");
+// newt.setAttribute("id", "para")
+// newt.innerText ="Kaise ho sare";
+// const body= document.getElementById("body")
+// body.append(newt)
+// console.log(newt.innerText)
+
+
+let data=["monitor","laptop","mouse","keayboard","mic"]
+const list = document.getElementById("list")
+
+// const litag= document.createElement("li")
+// litag.innerText="monitor"
+
+// list.append(litag)
+
+for(let i=0;i<data.length;i++){
+    const litag= document.createElement("li")
+    litag.style.backgroundColor="pink"
+    litag.style.margin=".8rem";
+    litag.style.height="2.5vh"
+    litag.style.padding="1.5rem"
+    litag.style.borderRadius="1.25rem"
+    litag.innerText=data[i]
+    list.append(litag)
+}
