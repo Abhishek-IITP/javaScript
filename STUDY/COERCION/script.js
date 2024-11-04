b0aeb1f892b515b62c6d13698c35ec4ff94880fb
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-11-04 18:43:26 +0530

    Update script.js

--- STUDY/COERCION/script.js
+++ STUDY/COERCION/script.js
@@ -1,1 +1,15 @@
-console.log('hello')
+/** THIS IS WHAT JS DO AUTOMATICALLY... TYPE OF C = STRING */
+
+// let a= 6;
+// let b= "5";
+// let c= a+b;
+// console.log(c)
+// console.log(typeof c)
+
+/** N0W I CHANGE THE TYPE MANUALLY AND THE TYPE OF C BECOME NUMBER*/
+
+let a= 6;
+let b= Number("5");
+let c= a+b;
+console.log(c)
+console.log(typeof c)
