b5a27a4394b3bcd4dfa148b6289fdf371eb6b6bd
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-11-05 18:12:02 +0530

    Update script.js

---
+++ STUDY/DOM/event propogation/script.js
@@ -0,0 +1,41 @@
+const mainDiv=document.getElementById("mainDiv");
+const box=document.getElementById("div1");
+/**bubbling */
+
+// box.addEventListener('click',()=>{
+//     console.log('box')
+// })
+// mainDiv.addEventListener('click',()=>{
+//     console.log('main')
+// })
+// document.body.addEventListener('click',()=>{
+//     console.log("body")
+// })
+
+/**event capturing */
+
+// box.addEventListener('click',()=>{
+//     console.log('box')
+// },true)
+// mainDiv.addEventListener('click',()=>{
+//     console.log('main')
+// },true)
+// document.body.addEventListener('click',()=>{
+//     console.log("body")
+// },true)
+
+/**to stop event propogation */
+
+box.addEventListener('click',(e)=>{
+    e.stopPropagation()
+    console.log('box')
+
+})
+mainDiv.addEventListener('click',(e)=>{
+    e.stopPropagation()
+    console.log('main')
+})
+document.body.addEventListener('click',(e)=>{
+    // e.stopPropagation()
+    console.log("body")
+})
