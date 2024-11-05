533576d6a8217959f3864f8a18f1d44760e7a5a5
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-11-05 18:12:12 +0530

    Create script.js

---
+++ STUDY/DOM/smallProject/Drag&Drop/script.js
@@ -0,0 +1,33 @@
+const paper1= document.querySelector('.paper1');
+const paper2= document.querySelector('.paper2');
+const item1= document.getElementsByClassName('item1');
+const body=document.getElementById('conatiner')
+
+
+for(let items of item1){
+    items.addEventListener('dragstart',(e)=>{
+        let data =e.target
+        paper2.addEventListener('dragover',(e)=>{
+            e.preventDefault();
+            e.stopPropagation();
+
+        })
+
+        paper2.addEventListener('drop',()=>{
+            paper2.append(data);
+            data=''
+        })
+
+
+        paper1.addEventListener('dragover',(e)=>{
+            e.preventDefault();
+            e.stopPropagation();
+
+        })
+
+        paper1.addEventListener('drop',()=>{
+            paper1.append(data);
+            data=''
+        })
+    })
+}
