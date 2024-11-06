f8b670d02899c1c87548f60679b51d4a740e95f8
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-11-06 11:36:22 +0530

    Update script.js

--- STUDY/DOM/smallProject/Drag&Drop/script.js
+++ STUDY/DOM/smallProject/Drag&Drop/script.js
@@ -1,33 +1,63 @@
-const paper1= document.querySelector('.paper1');
-const paper2= document.querySelector('.paper2');
-const item1= document.getElementsByClassName('item1');
-const body=document.getElementById('conatiner')
+const paper1=document.querySelector('.paper1');
+const paper2=document.querySelector('.paper2')
+const items=document.querySelectorAll('.item1');
 
 
-for(let items of item1){
-    items.addEventListener('dragstart',(e)=>{
-        let data =e.target
-        paper2.addEventListener('dragover',(e)=>{
+
+items.forEach((item) => {
+    console.log(item)
+    item.addEventListener('dragstart', (e) => {
+        let drop = e.target;
+        paper2.addEventListener('dragover', (e) => {
             e.preventDefault();
             e.stopPropagation();
-
-        })
-
-        paper2.addEventListener('drop',()=>{
-            paper2.append(data);
-            data=''
-        })
-
-
-        paper1.addEventListener('dragover',(e)=>{
+        });
+        paper2.addEventListener('drop', () => {
+            paper2.append(drop);
+            drop = '';
+        });
+        paper1.addEventListener('dragover', (e) => {
             e.preventDefault();
             e.stopPropagation();
+        });
+        paper1.addEventListener('drop', () => {
+            paper1.append(drop);
+            drop = '';
+        });
+    });
+});
 
-        })
 
-        paper1.addEventListener('drop',()=>{
-            paper1.append(data);
-            data=''
-        })
-    })
-}
+// const paper1= document.querySelector('.paper1');
+// const paper2= document.querySelector('.paper2');
+// const item1= document.getElementsByClassName('item1');
+// const body=document.getElementById('conatiner')
+
+
+// for(let items of item1){
+//     items.addEventListener('dragstart',(e)=>{
+//         let data =e.target
+//         paper2.addEventListener('dragover',(e)=>{
+//             e.preventDefault();
+//             e.stopPropagation();
+
+//         })
+
+//         paper2.addEventListener('drop',()=>{
+//             paper2.append(data);
+//             data=''
+//         })
+
+
+//         paper1.addEventListener('dragover',(e)=>{
+//             e.preventDefault();
+//             e.stopPropagation();
+
+//         })
+
+//         paper1.addEventListener('drop',()=>{
+//             paper1.append(data);
+//             data=''
+//         })
+//     })
+// }
