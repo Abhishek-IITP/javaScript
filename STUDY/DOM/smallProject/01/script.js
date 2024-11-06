c36d861cf506fbc05f1d5f4541aef0f26c964aa3
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-11-06 12:58:19 +0530

    Update script.js

--- STUDY/DOM/smallProject/01/script.js
+++ STUDY/DOM/smallProject/01/script.js
@@ -1,11 +1,11 @@
-const btn = document.getElementById('btn')
-const body = document.getElementById('container')
+// const btn = document.getElementById('btn')
+// const body = document.getElementById('container')
 
 const submit= document.getElementById('submit');
 const colorName= document.getElementById('colorname');
 
 submit.addEventListener("click",(e)=>{
     e.preventDefault();
-    body.style.backgroundColor=`${colorName.value}`;
-    
+    console.log(colorName.value)
+    document.body.style.backgroundColor=`${colorName.value}`;
 })
