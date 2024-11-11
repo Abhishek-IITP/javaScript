55dd2fe853c5b6821ed7f3ff4b2f634808d2a251
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-11-12 01:32:15 +0530

    new

--- setInterval_&_setTimeout/script.js
+++ setInterval_&_setTimeout/script.js
@@ -7,22 +7,23 @@
 // })
 
 
-// function fun1(text){
-//     var a=5;
-//     function fun2(){
-//         console.log(a)
-//     }
-//     a=50;  //aapne newest refernce ko hi lega yeah
-//     return fun2;
-// }
-// let abhi=fun1('from abhishek');
-// abhi()
-
 function fun1(text){
+    var a=5;
     function fun2(){
-        console.log(text)
+        console.log(a + " " + text)
     }
+    text="Yo... abhi this side"
+    a=50;  //aapne newest refernce ko hi lega yeah
     return fun2;
 }
 let abhi=fun1('from abhishek');
 abhi()
+
+// function fun1(text){
+//     function fun2(){
+//         console.log(text)
+//     }
+//     return fun2;
+// }
+// let abhi=fun1('from abhishek');
+// abhi()
