fa88a0cd1615eec09c66ee845d0f98a6c5835bc8
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-11-17 15:28:45 +0530

    update

--- Promises/script.js
+++ Promises/script.js
@@ -1,1 +1,15 @@
-console.log('first')
+
+/** state and value two type(result or v) of promises 
+ * state:-
+ * a) pending //initial state
+ * b)fulfilled also knows a complete , resolve
+ * c) rejected //both b and c are from final state
+ */
+/**
+ * value:-
+ * promise->pending -> initial state
+ * value:- Undefined
+ * 
+ * promise-> final state -> settled{yeah toh fulfilled ya phir reject}
+ * value:-return value
+ */
