51adcfe83dddaa4f55af3ef25ea1f5b869dfb2a9
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-11-23 18:39:23 +0530

    Update script.js

--- callBack Hell/script.js
+++ callBack Hell/script.js
@@ -31,3 +31,46 @@ let abhi=searchFood('burger' ,function fun1( item,data){
         })
     })
 })
+
+
+// function searchFood(item){
+//     return new Promise(function fun1(res,rej){
+//         console.log('searching start for',item,'......')
+//         setTimeout(function timer1(){
+//             let data=`list of ${item}`;
+//             res({item,data}); 
+//         },3000);
+//     })
+// }
+// function orderFood(item){
+//     return new Promise(function fun2(res,rej){
+//         console.log('select', item);
+//         setTimeout(function timer2() {
+//             let id = Math.floor(Math.random()*99999999);
+//             res(id);
+            
+//         }, 4000);
+//     })
+// }
+// function payments(item,id){
+//     return new Promise(function fun3(res,rej){
+//         console.log(`payment for ${item} with id no :- ${id}`);
+        
+//         setTimeout(() => {
+//             let status =true;
+//             res(status)
+//         }, 5000);
+//     })
+
+// }
+
+// let result =searchFood("pizza").then(function consumer1(item){
+//     console.log(item.data);
+//     orderFood(item.item).then(function consumer2(id){
+//         console.log('your order id is :-',id);
+//         payments(item.item,id).then(function consumer3(status){
+//             console.log(status);
+//         })
+//     });
+
+// });
