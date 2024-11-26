39fe04cab5e6341e1c2987a5b22a9b58b98d61d8
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-11-26 13:57:08 +0530

    "new commit"

--- Async_Await/script.js
+++ Async_Await/script.js
@@ -2,8 +2,8 @@ function searchFood(item){
     return new Promise(function fun1(res,rej){
             console.log('Searching start for', item , '.........')
             setTimeout(function timer1(){
-                let data = `list of ${item}`
-                res({item,data});
+                let Type = `list of ${item}`
+                res({item,Type});
                 
             },3000);
         
@@ -27,23 +27,38 @@ function payments(item,id){
             
     setTimeout(function timer3(){
         let status = true;
+        console.log("Payment successfull with status",status)
         res(status);
     }, 5000);
     })
 
 }
 
-let result =searchFood("pizza").then(function consumer1(item){
-    console.log(item.data);
-    return orderFood(item.item)
-})
-.then(function consumer2(id){
-    console.log("order created successfully with id no ",id.id);
-    return payments(id.item,id.id)
-})
-.then(function consumer3(status){
-    console.log(status)
-})
+async function foodOrder(item){
+    let res1 = await searchFood(item);
+    let res2= await orderFood(res1.item);
+    console.log("order created successfully with id no ",res2.id);
+    let res3= await payments(res2.item, res2.id)
+}
+foodOrder("Biryani")
+
+
+
+
+
+
+// let result =searchFood("pizza").then(function consumer1(item){
+//     console.log(item.data);
+//     return orderFood(item.item)
+// })
+// .then(function consumer2(id){
+//     console.log("order created successfully with id no ",id.id);
+//     return payments(id.item,id.id)
+// })
+// .then(function consumer3(status){
+//     console.log(status)
+// })
+
 
 // async function fun1(){
 //     console.log('first')
@@ -51,3 +66,38 @@ let result =searchFood("pizza").then(function consumer1(item){
 // }
 // let x= fun1()
 // console.log(x)
+
+
+// function fun2(){
+    // return Promise.resolve('hello')
+    // return new Promise(function(res,rej){
+    //     res('abhi')
+    // })
+
+// }
+// let y =fun2();
+// console.log(y)
+
+// async function fun1(){
+//     return "kumar"
+// }
+// fun1().then((data)=>{
+//     console.log(data)
+// })
+
+// function pro(){
+//     return new Promise(function pro1(res,rej){
+//         res('ABhishek')
+//     })
+// }
+
+// async function fun1() {
+//     let x = await pro()
+//     console.log(x)
+//     /**fun1() kuch return krega tb na .then wale portion me kuch output aayega */
+//     return "abhiiiiiii"
+// }
+
+// fun1().then((data)=>{
+//     console.log('abhi',data)
+// })
