9b63d9e71c89acde222f591105b1342d38962129
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-12-16 21:15:29 +0530

    Create script.js

---
+++ STUDY/OOPS/script.js
@@ -0,0 +1,16 @@
+let obj = {
+    username : "abhi",
+    email : "abhi@gmail.com",
+    printName(){
+        console.log(this.username)
+    },
+    
+    // printEmail : function(){
+            
+    // },
+    
+    // printName2: () => {
+            
+    // }
+}
+obj.printName();
