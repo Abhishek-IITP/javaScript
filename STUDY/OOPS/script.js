b36611882133f7d91a63a45701b83f85b3db863a
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-12-17 16:03:23 +0530

    Update script.js

--- STUDY/OOPS/script.js
+++ STUDY/OOPS/script.js
@@ -1,9 +1,9 @@
-let obj = {
-    username : "abhi",
-    email : "abhi@gmail.com",
-    printName(){
-        console.log(this.username)
-    },
+// let obj = {
+//     username : "abhi",
+//     email : "abhi@gmail.com",
+//     printName(){
+//         console.log(this.username)
+//     },
     
     // printEmail : function(){
             
@@ -12,5 +12,22 @@ let obj = {
     // printName2: () => {
             
     // }
+// }
+// obj.printName() ;
+
+
+function Products(name,price){
+    this.name=name;
+    this.price=price;
 }
-obj.printName();
+let prod1= new Products("samsung S24 ultra" , 120000);
+let prod2= new Products('iPhone 16 pro max', 150000);
+console.log(prod1)
+console.log(prod2)
+
+/** 'NEW' KEYWORDS 4 STEPS
+ * NEW EMPTY OBJECTS GET CREATED
+ * OBJECT GET LINKED TO PROTOTYPE OF THE CONSSTRUCTOR FUNCTION 
+ * OBJECT GET BOUNDS AS 'THIS'
+ * RETURN OBJECT
+  */
