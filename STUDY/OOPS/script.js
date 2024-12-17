cf84a8981c4edff05200953eafdd2248df85292e
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2024-12-17 16:08:58 +0530

    Update script.js

--- STUDY/OOPS/script.js
+++ STUDY/OOPS/script.js
@@ -4,6 +4,9 @@
 //     printName(){
 //         console.log(this.username)
 //     },
+
+const { pid } = require("process");
+
     
     // printEmail : function(){
             
@@ -19,11 +22,14 @@
 function Products(name,price){
     this.name=name;
     this.price=price;
+    this.productName= function(){
+        console.log(this.name)
+    }
 }
 let prod1= new Products("samsung S24 ultra" , 120000);
 let prod2= new Products('iPhone 16 pro max', 150000);
-console.log(prod1)
-console.log(prod2)
+console.log(prod1.productName())
+// console.log(prod2)
 
 /** 'NEW' KEYWORDS 4 STEPS
  * NEW EMPTY OBJECTS GET CREATED
@@ -31,3 +37,13 @@ console.log(prod2)
  * OBJECT GET BOUNDS AS 'THIS'
  * RETURN OBJECT
   */
+
+// class Products{
+//     constructor(name,price){
+//         this.name=name;
+//         this.price=price;
+//     }
+// }
+
+// let p1= new Products('nokia 3310' ,3000)
+// console.log(p1)
